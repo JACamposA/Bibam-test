355bf4ee018baac4d4947449dbcbe287dfd6518a
Jose Campos <josecampos_91@msn.com>
2019-09-10 18:35:06 -0300

    added search bar basics

---
+++ my-app/src/components/FlightSearch.js
@@ -0,0 +1,14 @@
+import React from "react"
+function FlightSearch() {
+    return (
+        <div class="FlightSearch">
+        <div class="div1"> </div>
+        <div class="div2"> </div>
+        <div class="div3"> </div>
+        <div class="div4"> </div>
+        <div class="div5"> </div>
+        </div>
+    )
+}
+
+export default FlightSearch
--- my-app/src/components/SectionOne.js
+++ my-app/src/components/SectionOne.js
@@ -3,11 +3,15 @@ import Title from './Title';
 import '../styles/Title.css';
 import TravelType from './TravelType';
 import '../styles/TravelType.css';
+import FlightSearch from './FlightSearch';
+import '../styles/FlightSearch.css';
+
 function SectionOne() {
     return (
             <section className="Section1">
                 <Title/>
                 <TravelType/>
+                <FlightSearch/>
             </section>
     )
 }
