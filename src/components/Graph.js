938dad13249cfeb57df8493f25ab303773ef751d
Jose Campos <josecampos_91@msn.com>
2019-09-13 13:52:32 -0300

    started on sectioon 2

---
+++ src/components/Graph.js
@@ -0,0 +1,9 @@
+import React from "react"
+import '../styles/Graph.css';
+function Graph() {
+    return (
+            <div class="graph"></div>
+    )
+}
+
+export default Graph
--- src/components/SectionTwo.js
+++ src/components/SectionTwo.js
@@ -1,8 +1,11 @@
 import React from "react"
+import Graph from "./Graph"
+import '../styles/Graph.css';
+
 function SectionTwo() {
     return (
             <section className="Section2">
-                
+            <Graph/>
             </section>
     )
 }
